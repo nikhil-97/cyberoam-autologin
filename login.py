c509d9e5b11ed207e04bf4a04e3c41a9b1b30424
P V Nikhilanj <nikhilanj97@gmail.com>
2018-04-09 14:51:48 +0530

    updated login.py with logging,etc.

--- login.py
+++ login.py
@@ -1,54 +1,55 @@
-import urllib.request
+import requests
 import re
 import random
-import time 
+import time
+import logging
 
-HOST_URL = 'host_login_url'
-USERNAME_LIST = []  #without prefix
-USERNAME_PREFIX = "default_prefix"
-CHOICE = ''
+HOST_URL = 'http://172.16.0.30:8090/login.xml'
 PASSWORD = "default_password"
 
-def tryUser(USERNAME_LIST):
+def tryUserLogin(username,password):
     try:
-            req = urllib.request.Request(host_url)
-            data = 'mode=191&username='USERNAME_PREFIX+USERNAME_LIST+'&password='+PASSWORD+'&a=1355344698415' 
-            data = data.encode('utf-8')
-            get = urllib.request.urlopen(host_url,data)
-            getData = get.read()
-            getData = getData.decode('ascii')
-            message = re.findall(r'<message>(.*?)</message>',str(getData))
+            a_param = str(int(time.time()*1000))
+            credentials = {'mode':'191','username':username,'password':password,'a':a_param}
+            post_response = requests.post(HOST_URL,data = credentials)
+            postData = post_response.content
+            message = re.findall(r'<message>(.*?)</message>',str(postData))
             return message[0]
         
-    except urllib.error.URLError as err:
+    except (requests.ConnectionError,requests.HTTPError) as err:
             print(str(err))
 
 num = 0
 # Make a temporary list of the Usernames
-tempList = username
+credsDict = {'f2014855':'blackberry29!','f2013358':'Dreimar@123','f2013408':'pussycat123@'}
+
+logging.basicConfig(filename='cyberoam_login_logging.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
 while(1):
     try:
-        # Randomly pick usernames and distribute load
-        choice = random.choice(tempList)
+    # Randomly pick usernames and distribute load
+        random_username = random.choice(list(credsDict.keys()))
+        pwd = credsDict[random_username]
+        print random_username
+        logging.info('Chosen username = '+random_username)
         try:
-            message = tryUser(choice)
+            message = tryUserLogin(random_username,pwd)
+            logging.debug(message)
             if(message == '<![CDATA[You have successfully logged in]]>'):
-                print('Logged In As ' + 'f201'+choice)
+                print('Logged In As ' + random_username)
                 break
             else:
                 if(message == '<![CDATA[Your data transfer has been exceeded, Please contact the administrator]]>'):
-                    print('Connection Failed for '+USERNAME_PREFIX+CHOICE+'. Data Limit has already exceeded.')
+                    print('Connection Failed for '+username+' Data Limit has already exceeded.')
                 if(message == '<![CDATA[The system could not log you on. Make sure your password is correct]]>'):
-                    print('Connection Failed for '+USERNAME_PREFIX+CHOICE+' due to incorrect password.')
+                    print('Connection Failed for '+username+' due to incorrect password.')
                 num=num+1
-            # Pop incorrect choice from list
-            tempList.remove(choice)
-            # Optional time delay
-            time.sleep(0.1)
+            time.sleep(3)
         except:
             # Exhausted our list and couldn't log in
-            print("End of list reached. Couldn't log in :(")
+            logging.error("End of list reached. Couldn't log in :(. Supply new usernames and passwords")
             break
     except:
-        # Network error
-        print("Login failed due to network error. Please try again.")
+            # Network error
+        #print("Login failed due to network error. Please try again.")
+        logging.critical("Login failed due to network error. Please try again.")
